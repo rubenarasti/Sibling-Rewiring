91b6f05f411e167aa95dbb291034d050575bf474
María Ojeda <mor1001@alu.ubu.es>
2020-12-26 18:55:32 +0100

    Created html login
    Creado el login en html, faltando la conexión con la base de datos así como el registro de usuarios.

--- Environment/sire/sire.py
+++ Environment/sire/sire.py
@@ -3,7 +3,6 @@ from database import *
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     title = "Sire"
@@ -16,5 +15,34 @@ def register_user():
     insert_user()
 register_user()
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/authentication',methods=['POST','GET'])
+def authenticate():
+   if request.method == 'POST':
+       uname = request.form['username']
+       passwrd = request.form['password']   
+       cur = mysql.connection.cursor()
+       cur.execute("SELECT username,password FROM user WHERE username=%s",[uname])
+       user = cur.fetchone()
+       temp = user[1]
+       if len(user) > 0:
+           session.pop('username',None)
+           if (bcrypt.check_password_hash(temp,passwrd)) == True:  
+               session['username'] = request.form['username']
+               return render_template('home.html',uname=uname)
+           else:
+               flash('Invalid Username or Password !!')
+               return render_template('login.html')
+   else:
+       return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+   session.clear()
+   return render_template('login.html')
+   
 if __name__ == '__main__':
     app.run()
