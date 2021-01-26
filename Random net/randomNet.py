e31513d78e39de5ff396c90ddc443d30e699a6bc
María Ojeda <mor1001@alu.ubu.es>
2021-01-26 14:42:43 +0100

    Modified initial network to make equalitary
    Se ha modificado la red inicial para hacer que el número de alumnos por clase sea el mismo.

--- Random net/randomNet.py
+++ Random net/randomNet.py
@@ -3,48 +3,64 @@ import random
 import pandas as pd
 from matplotlib import pyplot as plt
 
-totalNodos = 200
+totalAlumnos = 975
 G = nx.Graph()
 siblings = []
 
 def create_initial_network():
     initial_network = nx.Graph()
-    initial_network.add_nodes_from(range(totalNodos))
+    initial_network.add_nodes_from(range(totalAlumnos))
 
     dicNombre = {}
     dicEtapa = {}
     dicCurso = {}
     dicClase = {}
-    nodos = list(initial_network.nodes())
-    curso = 0
     etapa = (['infantil', 'primaria', 'secundaria'])
     clase = (['A', 'B', 'C'])
     
-    for x in nodos:
-        selectedEtapa= random.choice(etapa)
-        
-        if selectedEtapa == 'infantil':
-            curso = random.randint(1,3)
-        elif selectedEtapa == 'primaria':
-            curso = random.randint(1,6)
-        else: 
-            curso = random.randint(1,4)  
-        
-        selectedClase = random.choice(clase)
-        
-        dicNombre[x] = x
-        dicEtapa[x] = selectedEtapa
-        dicCurso[x] = curso
-        dicClase[x] = selectedClase
-        
+    
+    alumnos_clase = totalAlumnos//(3*len(clase)+6*len(clase)+4*len(clase))
+    
+    x = 0
+    for et in etapa:
+        letra = 0
+        if et == etapa[0]:
+            for curso in range(1,4):
+                for alumno1 in range(alumnos_clase):
+                    for letra in clase:
+                        dicEtapa[x] = et
+                        dicCurso[x] = curso
+                        dicClase[x] = letra 
+                        dicNombre[x] = x
+                        x+=1
+        elif et==etapa[1]:
+            for curso in range(1,7):
+                for alumno2 in range(alumnos_clase):
+                    for letra in clase:
+                        dicEtapa[x] = et
+                        dicCurso[x] = curso
+                        dicClase[x] = letra 
+                        dicNombre[x] = x
+                        x += 1
+        elif et==etapa[2]:
+            for curso in range(1,5):
+                for alumno3 in range(alumnos_clase):
+                    for letra in clase:
+                        #print('etapa', et,'curso', curso,'clase', letra, 'alumno', x,sep=',')
+                        dicEtapa[x] = et
+                        dicCurso[x] = curso
+                        dicClase[x] = letra 
+                        dicNombre[x] = x
+                        x += 1
+            
+            
     nx.set_node_attributes(initial_network, dicNombre, 'Nombre')
     nx.set_node_attributes(initial_network, dicEtapa, 'Etapa')
     nx.set_node_attributes(initial_network, dicCurso, 'Curso')
     nx.set_node_attributes(initial_network, dicClase, 'Clase')
     
-    
     copy = list(initial_network.nodes())
-    num = 10
+    num = 75
     for i in range(0,num):
         node1 = random.choice(copy)
         node2 = random.choice(copy)
@@ -58,12 +74,12 @@ def create_initial_network():
     nx.draw(initial_network, pos)
     node_labels = nx.get_node_attributes(initial_network,'Nombre')
     nx.draw_networkx_labels(initial_network, pos, labels = node_labels)
-    node_labels1 = nx.get_node_attributes(initial_network,'Etapa')
-    nx.draw_networkx_labels(initial_network, pos, labels = node_labels1)
-    node_labels2 = nx.get_node_attributes(initial_network,'Curso')
-    nx.draw_networkx_labels(initial_network, pos, labels = node_labels2)
-    node_labels3 = nx.get_node_attributes(initial_network,'Clase')
-    nx.draw_networkx_labels(initial_network, pos, labels = node_labels3)
+    #node_labels1 = nx.get_node_attributes(initial_network,'Etapa')
+    #nx.draw_networkx_labels(initial_network, pos, labels = node_labels1)
+    #node_labels2 = nx.get_node_attributes(initial_network,'Curso')
+    #nx.draw_networkx_labels(initial_network, pos, labels = node_labels2)
+    #node_labels3 = nx.get_node_attributes(initial_network,'Clase')
+    #nx.draw_networkx_labels(initial_network, pos, labels = node_labels3)
     
     plt.show()
     
@@ -73,6 +89,7 @@ def create_initial_network():
     nx.write_gexf(initial_network, "randomGraphuploaded.gexf")
     
     return initial_network
+   
 
 def create_siblings_matrix():
     initial = nx.Graph()
@@ -165,8 +182,9 @@ def create_schoolyear_class_network(G):
         
     print('enlaces de schoolyear_class')
     print(schoolyear_class.edges(data=True))
+    print(len(schoolyear_class.edges()))
     
-    pos=nx.kamada_kawai_layout(schoolyear_class)
+    pos=nx.circular_layout(schoolyear_class)
     
     nx.draw(schoolyear_class, pos)
     node_labels = nx.get_node_attributes(schoolyear_class,'Nombre')
@@ -176,7 +194,9 @@ def create_schoolyear_class_network(G):
     
     plt.show()
     
-G, siblings = create_siblings_matrix()
-create_schoolyear_class_network(G)
-#print(siblings)
+G, siblings_matrix = create_siblings_matrix()
+print(siblings_matrix)
+print(siblings)
 #print(siblings.values)
+#create_schoolyear_class_network(G)
+#create_initial_network()
